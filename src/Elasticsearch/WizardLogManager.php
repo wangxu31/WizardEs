3d37a5b594f85767ec3da5d39573f56827135c64
Clive <874760401@qq.com>
2019-09-06 17:01:02 +0800

    ad log manager

--- src/Elasticsearch/WizardLogManager.php
+++ src/Elasticsearch/WizardLogManager.php
@@ -6,10 +6,6 @@ namespace Wizard\Elasticsearch;
  * @author wangxu <wxu@wooduan.com>
  */
 
-use CrCms\ElasticSearch\Grammar;
-use Elasticsearch\ClientBuilder;
-use CrCms\ElasticSearch\Builder;
-
 class WizardLogManager extends WizardEsManager
 {
     public function __construct(array $configs, int $retries = 3)
@@ -17,11 +13,50 @@ class WizardLogManager extends WizardEsManager
         parent::__construct($configs, $retries);
     }
 
-    public function log(string $level='info', array $logData=[], \Exception $e=null){
+    public function debug(array $logData=[], \Exception $e=null){
         if (count($logData) != count($logData,1)) {
             throw new \Exception('Log data should be one dimension array');
         }
-        $logData['log_level'] = $level;
+        $logData['log_level'] = 'debug';
+        if (!is_null($e)) {
+            $logData['exception_code']  = $e->getCode();
+            $logData['exception_msg']   = $e->getMessage();
+            $logData['exception_trace'] = json_encode($e);
+        }
+        return $this->createDoc($logData);
+    }
+
+    public function warning(array $logData=[], \Exception $e=null){
+        if (count($logData) != count($logData,1)) {
+            throw new \Exception('Log data should be one dimension array');
+        }
+        $logData['log_level'] = 'warning';
+        if (!is_null($e)) {
+            $logData['exception_code']  = $e->getCode();
+            $logData['exception_msg']   = $e->getMessage();
+            $logData['exception_trace'] = json_encode($e);
+        }
+        return $this->createDoc($logData);
+    }
+
+    public function info(array $logData=[], \Exception $e=null){
+        if (count($logData) != count($logData,1)) {
+            throw new \Exception('Log data should be one dimension array');
+        }
+        $logData['log_level'] = 'info';
+        if (!is_null($e)) {
+            $logData['exception_code']  = $e->getCode();
+            $logData['exception_msg']   = $e->getMessage();
+            $logData['exception_trace'] = json_encode($e);
+        }
+        return $this->createDoc($logData);
+    }
+
+    public function error(array $logData=[], \Exception $e=null){
+        if (count($logData) != count($logData,1)) {
+            throw new \Exception('Log data should be one dimension array');
+        }
+        $logData['log_level'] = 'error';
         if (!is_null($e)) {
             $logData['exception_code']  = $e->getCode();
             $logData['exception_msg']   = $e->getMessage();
